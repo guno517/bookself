092d6a8a1625be6674293cc5ffa1df47db8099a5
guno517 <guno5170@gmail.com>
2022-04-07 12:10:47 +0900

    GlobalStyle 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
+import { Global } from "@emotion/react";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import DetailPage from "./pages/DetailPage";
 import IndexPage from "./pages/IndexPage";
+import reset from "./styles/GlobalStyle";
 
-function App() {
+const App = () => {
   return (
     <BrowserRouter>
+      <Global styles={reset} />
       <Routes>
         <Route path="/" element={<IndexPage />} />
         <Route path="/:id" element={<DetailPage />} />
       </Routes>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,11 @@
+import { css } from "@emotion/react";
+
+const reset = css`
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+`;
+
+export default reset;
