b7f2a7e48ad434237fad90de6f396d0f6a68a8eb
guno517 <guno5170@gmail.com>
2022-04-11 18:17:01 +0900

    Pokemon Detail page

---
+++ src/components/PokemonInfo.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const PokemonInfo: React.FC = () => {
+  return <Base>detail page</Base>;
+};
+
+export default PokemonInfo;
+
+const Base = styled.div``;
---
+++ src/utils.ts
@@ -0,0 +1,16 @@
+export const mapColorToHex = (color: string) => {
+  switch (color) {
+    case 'white':
+    case 'gray':
+      return '#6B7280';
+    case 'brown': return '#92400E'
+    default: return '#6B7280'
+  }
+}
+
+export const mapTypeToHex = (type: string) => {
+  switch (type) {
+    case 'bug': return '#92BC2C';
+    default: return '#6B7280'
+  }
+}
