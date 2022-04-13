d76f9fb40bf013b9ed303cadb2ec8f1a865baa75
guno517 <guno5170@gmail.com>
2022-04-13 16:41:26 +0900

    CSS: emotion 적용

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,13 +1,16 @@
 // 포켓몬 목록 페이지
 import React from "react";
+import styled from "@emotion/styled";
 import PokemonList from "../components/PokemonList";
 
 const IndexPage: React.FC = () => {
   return (
-    <div>
+    <Base>
       <PokemonList />
-    </div>
+    </Base>
   );
 };
 
 export default IndexPage;
+
+const Base = styled.div``;
