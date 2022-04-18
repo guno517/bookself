a59cb1b662c9357bc5a126a8f7f51f8ce5fe2f1a
guno517 <guno5170@gmail.com>
2022-04-18 20:26:20 +0900

    Tab 컴포넌트 생성

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,14 +1,27 @@
 // 포켓몬 상세 페이지
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
+import Tabs from "../components/Tabs";
 
 type Params = {
   id: string;
 };
 
+type Tab = "about" | "stats" | "evolution";
+
 const DetailPage: React.FC = () => {
   const { id } = useParams<Params>();
-  return <div>detail {id}</div>;
+  const [selectedTab, setSelectedTab] = useState<Tab>("about");
+
+  const handleClick = (tab: Tab) => {
+    setSelectedTab(tab);
+  };
+
+  return (
+    <div>
+      <Tabs tab={selectedTab} onClick={handleClick} />
+    </div>
+  );
 };
 
 export default DetailPage;
