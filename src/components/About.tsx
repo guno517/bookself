ee1ff3384bd7d5c74881ca786758d36cd933dc24
guno517 <guno5170@gmail.com>
2022-04-22 16:37:27 +0900

    CSS: 상세페이지 about tab css

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -104,24 +104,66 @@ const TypeWrapper = styled.div<{ color: string }>`
 const TypeList = styled.div`
   display: flex;
   margin-top: 8px;
+  ${TypeWrapper} + ${TypeWrapper} {
+    margin-left: 8px;
+  }
 `;
 
-const TypeImage = styled.img``;
+const TypeImage = styled.img`
+  height: 12px; ;
+`;
 
-const TypeLabel = styled.div``;
+const TypeLabel = styled.span`
+  margin-left: 4px;
+  color: #fff;
+  font-size: 10px;
+`;
 
-const ImageWrapper = styled.div``;
+const ImageWrapper = styled.div`
+  width: 100%;
+  height: 160px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
-const Image = styled.img``;
+const Image = styled.img`
+  width: 120px;
+  height: 120px;
+  object-fit: contain;
+`;
 
-const InfoContainerWrapper = styled.div``;
+const InfoContainerWrapper = styled.div`
+  margin-top: 32px;
+`;
 
-const Title = styled.h4``;
+const Title = styled.h4<{ color: string }>`
+  margin: 0;
+  padding: 0;
+  font-size: 20px;
+  font-weight: bold;
+  color: ${({ color }) => color};
+`;
 
-const InfoContainer = styled.div``;
+const InfoContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat() (2, 1fr);
+  margin-top: 20px;
+  row-gap: 12px;
+`;
 
-const InfoItem = styled.div``;
+const InfoItem = styled.div`
+  display: grid;
+  grid-template-columns: repeat()(2, minmax((0, 1fr));
+`;
 
-const InfoItemLabel = styled.span``;
+const InfoItemLabel = styled.span`
+  font-weight: bold;
+  color: #374151;
+  font-size: 12px;
+`;
 
-const InfoItemValue = styled.span``;
+const InfoItemValue = styled.span<{ color: string }>`
+  color: ${({ color }) => color};
+  font-size: 12px;
+`;
