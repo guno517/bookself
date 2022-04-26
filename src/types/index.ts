1609e2932d204fb6a47c3241a1b3d9edf7559192
guno517 <guno5170@gmail.com>
2022-04-26 13:35:30 +0900

    Type 추가

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,35 @@
+export type Sprites = {
+  back_default: string;
+  back_shiny: string;
+  front_default: string;
+  front_shiny: string;
+  other: {
+    dream_world: {
+      front_default: string;
+    }
+    'official-artwork': {
+      front_default: string;
+    }
+  }
+}
+
+export type Type = {
+  slot: number;
+  type: {
+    name: string;
+    url: string;
+  }
+}
+
+export type Stat = {
+  base_stat: number;
+  effort: number;
+  stat: {
+    name: string;
+    url: string;
+  }
+}
+
 export type Ability = {
   ability: {
     name: string;
@@ -7,47 +39,41 @@ export type Ability = {
   slot: number;
 }
 
-export type Height = {
-  height: number;
+export type Language = {
+  name: string;
+  url: string;
 }
 
 export type Name = {
+  language: Language;
   name: string;
 }
 
-export type Sprites = { // pokemon image
-  other: {
-    'officail-artwork': {
-      'front_default': string;
-    }
-  }
+export type Color = {
+  name: string;
+  url: string;
 }
 
-export type Stats = {
-  base_stat: number;
-  effort: number;
-  stat: {
-    name: string;
-    url: string;
-  }
+export type Version = {
+  name: string;
+  url: string;
 }
 
-export type Types = {
-  slot: number;
-  type: {
-    name: string;
-    url: string;
-  }
-  weight: number;
+export type FlavorTextEntry = {
+  flavor_text: string;
+  language: Language;
+  version: Version;
 }
 
-export type PokemonResponse = {
-
+export type GrowthRate = {
+  name: string;
+  url: string;
 }
 
-export type ListResponse = {
-  count: number;
-  results: Array<SimplePokemonInfo>
+export type EffectEntry = {
+  effect: string;
+  language: Language;
+  short_effect: string;
 }
 
 export type SimplePokemonInfo = {
@@ -55,25 +81,89 @@ export type SimplePokemonInfo = {
   url: string;
 }
 
-export type SimplePokemonName = {
-  names: [
-    {
-      language: {
-        name: string;
-        url: string;
-      }
-      name: string
-    }
-  ]
+export type DamageRelation = {
+  double_damage_from: Array<{ name: string, url: string }>;
+  double_damage_to: Array<{ name: string, url: string }>;
+  half_damage_from: Array<{ name: string, url: string }>;
+  half_damage_to: Array<{ name: string, url: string }>;
 }
 
-export type Type = {
-  type: {
+export type EvolutionDetail = {
+  min_level: number;
+  trigger: {
     name: string;
-  };
+    url: string;
+  }
 }
 
-export type Color = {
+export type Chain = {
+  is_baby: boolean;
+  evolution_details: Array<EvolutionDetail>;
+  evolves_to: Array<EvolutionTo>;
+  species: {
+    name: string;
+    url: string;
+  }
+}
+
+export type EvolutionTo = {
+  evolution_details: Array<EvolutionDetail>;
+  is_baby: boolean;
+  evolves_to: Array<EvolutionTo>;
+  species: {
+    name: string;
+    url: string;
+  }
+}
+export type ListResponse = {
+  count: number;
+  results: Array<SimplePokemonInfo>;
+}
+
+export type PokemonResponse = {
+  id: number;
   name: string;
-  url: string;
+  order: number;
+  sprites: Sprites;
+  base_experience: number;
+  height: number;
+  weight: number;
+  stats: Array<Stat>;
+  abilities: Array<Ability>;
+  types: Array<Type>;
+}
+
+export type SpeciesResponse = {
+  id: number;
+  name: string;
+  order: number;
+  names: Array<Name>;
+  color: Color;
+  flavor_text_entries: Array<FlavorTextEntry>;
+  growth_rate: GrowthRate;
+  gender_rate: number;
+  is_legendary: boolean;
+  is_mythical: boolean;
+  evolution_chain: {
+    url: string;
+  }
+}
+
+export type AbilityResponse = {
+  id: number;
+  name: string;
+  names: Array<Name>;
+  is_main_series: boolean;
+  effect_entries: Array<EffectEntry>;
+}
+
+export type TypeResponse = {
+  id: number;
+  name: string;
+  damage_relations: DamageRelation;
+}
+
+export type EvolutionChainResponse = {
+  id: number;
+  chain: Chain;
 }
